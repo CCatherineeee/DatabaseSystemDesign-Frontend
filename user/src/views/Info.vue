13de23397fec2c596380265771b16d7858212d47
CCCatherine <2870375520@qq.com>
2021-07-02 16:32:53 +0800

    user

---
+++ user/src/views/Info.vue
@@ -0,0 +1,37 @@
+<template>
+<el-form ref="form" :model="form" label-width="80px">
+  <el-form-item label="个人昵称">
+    <el-input v-model="form.name"></el-input>
+  </el-form-item>
+
+  <el-form-item label="出生年月">
+    <el-col :span="11">
+      <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
+    </el-col>
+  </el-form-item>
+  
+</el-form>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        }
+      }
+    },
+    methods: {
+      onSubmit() {
+        console.log('submit!');
+      }
+    }
+  }
+</script>
---
+++ user/src/views/activity.vue
@@ -0,0 +1,20 @@
+<template>
+
+<el-row>
+  <el-col :span="4" v-for="(o, index) in 4" :key="o" :offset="index > 0 ? 2 : 0">
+    <el-card :body-style="{ padding: '0px' }">
+      
+      <div style="padding: 14px;">
+        <span></span>
+        <h4>帖子名</h4>
+        <p>主楼内容</p>
+        <div class="bottom clearfix">
+          <time class="time">{{ currentDate }}</time>
+          <el-button type="text" class="button">打开帖子</el-button>
+        </div>
+      </div>
+    </el-card>
+  </el-col>
+</el-row>
+
+</template>
